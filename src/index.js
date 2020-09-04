58596c8f308173bfe73f75db9494093402eb11d8
edwinisnow <edwin04@gmail.com>
2020-09-05 00:04:31 +0100

    observable, action

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { useStrict } from 'mobx';
+import { observable, action, autorun } from 'mobx'
+class ShoppingCart {
+  @observable
+  items = [];
 
-useStrict(true)
+  @action
+  addItem(item) {
+    this.items.push(item)
+  }
+}
+
+const shoppingCart = new ShoppingCart()
+shoppingCart.addItem({ id: 3, name: "Computer", price: 3000 })
+shoppingCart.addItem({ id: 10, name: "Mouse", price: 50 })
+
+autorun(() => {
+  console.log(shoppingCart.items.length)
+  console.log(shoppingCart.items.map(item => item.name))
+})
 
 ReactDOM.render(
   <React.StrictMode>
