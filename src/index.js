1e157dc900048d5e7402c24403e06aee4e0d1d3b
edwinisnow <edwin04@gmail.com>
2020-09-05 22:59:52 +0100

    computed values

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { observable, action, autorun } from 'mobx'
+import { observable, action, autorun, computed } from 'mobx'
 class ShoppingCart {
   @observable
   items = [];
+
+  @computed
+  get totalPrice() {
+    console.log('totalPrice called')
+    return this.items.reduce((acc, curr) => acc + curr.price, 0)
+  }
 
   @action
   addItem(item) {
@@ -21,6 +27,7 @@ shoppingCart.addItem({ id: 10, name: "Mouse", price: 50 })
 autorun(() => {
   console.log(shoppingCart.items.length)
   console.log(shoppingCart.items.map(item => item.name))
+  console.log('totalPric ', shoppingCart.totalPrice)
 })
 
 ReactDOM.render(
